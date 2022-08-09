3d741c41abafee59f2f227b20749e804fd287333
AntonyBlakey <antony.blakey@gmail.com>
2022-08-09 13:31:55 +0000

    Comments, docker, docker-compose

--- shell.nix
+++ shell.nix
@@ -1,12 +1,19 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/9f15d6c3a74d2778c6e1af67947c95f100dc6fd2.tar.gz") { } }:
+# latest commit to nixpkgs-unstable as of 2022-08-09
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/9f15d6c3a74d2778c6e1af67947c95f100dc6fd.tar.gz") { } }:
 
 pkgs.mkShell {
-  # nativeBuildInputs is usually what you want -- tools you need to run
-  nativeBuildInputs = with pkgs; [
-    nixpkgs-fmt
-    rnix-lsp
-    nodejs
-    yarn
-    yarn-bash-completion
+  nativeBuildInputs = [
+    # For nix
+    pkgs.nixpkgs-fmt
+    pkgs.rnix-lsp
+
+    # For building
+    pkgs.nodejs-18_x
+    pkgs.yarn
+    pkgs.yarn-bash-completion
+
+    # For running
+    pkgs.docker-client
+    pkgs.docker-compose
   ];
 }
