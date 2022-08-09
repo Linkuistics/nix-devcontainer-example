ade9424751785d5f071f09a1286fcea5ca8230eb
AntonyBlakey <antony.blakey@gmail.com>
2022-08-09 12:22:39 +0000

    pinx nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/9f15d6c3a74d2778c6e1af67947c95f100dc6fd2.tar.gz") { } }:
 
 pkgs.mkShell {
   # nativeBuildInputs is usually what you want -- tools you need to run
